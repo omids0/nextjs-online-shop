8628c56fb8c140fdbf140103fb56980525811a06
omids0 <soheilniao@gmail.com>
2022-03-25 16:36:46 +0430

    header color changed

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,27 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 export default function Header() {
   return (
     <header>
-      <button className="login-btn">ورود</button>
-      <Image src='/images/—Pngtree—online shopping sale vector _3548364.png' width={80} height={80} alt='onlineshope-logo'/>
+      <div className="header-first-section">
+        <button className="login-btn">|||</button>
+        <Image
+          src="/images/—Pngtree—online shopping sale vector _3548364.png"
+          width={80}
+          height={80}
+          alt="onlineshope-logo"
+        />
+      </div>
+      <div className="header-second-section">
+        <div>
+          <input type="text" className="" placeholder="جستجو" />
+        </div>
+        <div>
+          <button className="btn">ورود</button>
+          <button>sabad</button>
+        </div>
+      </div>
     </header>
-  )
+  );
 }
--- pages/admin/index.js
+++ pages/admin/index.js
@@ -4,9 +4,8 @@ import Adminpage from "../../components/adminpage/Adminpage";
 import AppLayout from "../../components/AppLayout";
 
 export default function index() {
-
   return (
-    <AppLayout>
+    <AppLayout title="AdminPage" description='صفحه مدیریت سایت اعم از تعریف اجناس، بررسی کاربران و سفارشات'>
       <Adminpage />
     </AppLayout>
   );
