84df59538fd4c37d2a846d76424bfe39eee2ce8a
omids0 <soheilniao@gmail.com>
2022-03-26 14:14:53 +0430

    header icones added

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,21 @@ export default function Header() {
   return (
     <header>
       <div className="header-first-section">
-        <button className="login-btn">|||</button>
+        <button className="btn">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="currentColor"
+            className="bi bi-list"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+            />
+          </svg>
+        </button>
         <Image
           src="/images/—Pngtree—online shopping sale vector _3548364.png"
           width={80}
@@ -14,12 +28,15 @@ export default function Header() {
         />
       </div>
       <div className="header-second-section">
-        <div>
-          <input type="text" className="" placeholder="جستجو" />
+        <div className='header-part-section1'>
+          <input className="header-search" type="text" placeholder="جستجو" />
         </div>
         <div>
           <button className="btn">ورود</button>
-          <button>sabad</button>
+          {` | `}
+          <button className="cart-btn">
+            <i className="bi bi-cart sabad"></i>
+          </button>
         </div>
       </div>
     </header>
