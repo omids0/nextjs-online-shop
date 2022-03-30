e8ed5a4a82cde44049a6378f6b3ec704c2aa300c
omids0 <soheilniao@gmail.com>
2022-03-30 18:45:08 +0430

    first slide desine improved

--- components/homepage/Firstslide.js
+++ components/homepage/Firstslide.js
@@ -1,61 +1,132 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Firstslide({ products }) {
   const [index, setindex] = useState(0);
-  let interval = null;
+  const [loading, setloading] = useState(true);
+  const [startslide, setstartslide] = useState(true);
+
+  useEffect(() => {
+    if (startslide && index <= 4) {
+      setTimeout(() => {
+        setindex((index += 1));
+      }, 5000);
+    }
+    if (index === 4) {
+      setstartslide(false);
+    }
+  }, [index, startslide]);
 
   //functions
+  function nextSlide(slideNum) {
+    setstartslide(false);
+    if (index >= 0 || index < 5) {
+      setindex((index += slideNum));
+    }
+  }
+
+  function selectedDot(dot) {
+    setstartslide(false);
+    setindex(dot);
+  }
 
   return (
     <div className="homepage-first-slide">
-      <div className="first-slide-body">
-        <div className="first-slide-body-right">
-          <h1 className="first-slide-title">نام کیف</h1>
-          <p className="first-slide-description">توضیحات کیف</p>
-          <h3 className="first-slide-price">1000 تومان</h3>
-        </div>
-        <div className="first-slide-body-left">
-          <Image
-            src="/images/275876663_3068808890049026_5903621737662120503_n.jpg"
-            width={1000}
-            height={1000}
-            alt="اسلاید جدیدترین های امیدشاپ"
-            className="first-slide-image"
-          />
-        </div>
-        <div className="first-slide-mobile-view">
-          <div className="first-slide-body-mobile fade">
-            <Image
-              src="/images/275876663_3068808890049026_5903621737662120503_n.jpg"
-              width={1000}
-              height={1000}
-              alt="اسلاید جدیدترین های امیدشاپ"
-              className="first-slide-image"
-            />
-            <div className="slide1-text">
-              <h3 className="first-slide-title">نام کیفففففففففففف</h3>
-              <p className="first-slide-description">توضیحات کیف</p>
-              <p className="first-slide-price">1000 تومان</p>
+      {loading && (
+        <div>
+          <div className="first-slide-body">
+            <div className="first-slide-desktop">
+              <div className="first-slide-body-right">
+                <div>
+                  <h3 className="first-slide-title">{products[index].name}</h3>
+                  <h4 className="first-slide-price">{`${products[index].price} تومان`}</h4>
+                </div>
+                <p className="first-slide-description">
+                  {products[index].description}
+                </p>
+              </div>
+              <div className="first-slide-body-left">
+                <Image
+                  src={products[index].selectedImage}
+                  width={1000}
+                  height={1000}
+                  alt="اسلاید جدیدترین های امیدشاپ"
+                  className="first-slide-image"
+                />
+              </div>
+            </div>
+            <div className="first-slide-mobile-view">
+              <div className="first-slide-body-mobile fade">
+                <Image
+                  src={products[index].selectedImage}
+                  width={1000}
+                  height={1000}
+                  alt="اسلاید جدیدترین های امیدشاپ"
+                  className="first-slide-image-mobile"
+                />
+                <div className="slide1-text">
+                  <p className="first-slide-description-mobile">
+                    {products[index].description}
+                  </p>
+                  <div>
+                    <h5 className="first-slide-title-mobile">
+                      {products[index].name}
+                    </h5>
+                    <p className="first-slide-price-mobile">{`${products[index].price} تومان`}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="slide-controler">
+            <div>
+              <button
+                className="btn slide-arrow"
+                disabled={index === 0 && "false"}
+                onClick={() => nextSlide(-1)}
+              >
+                {"<"}
+              </button>
+            </div>
+            <div>
+              <span
+                className={index === 0 ? "dotselected dot" : "dot"}
+                onClick={() => selectedDot(0)}
+              ></span>
+              <span
+                className={index === 1 ? "dotselected dot" : "dot"}
+                onClick={() => selectedDot(1)}
+              ></span>
+              <span
+                className={index === 2 ? "dotselected dot" : "dot"}
+                onClick={() => selectedDot(2)}
+              ></span>
+              <span
+                className={index === 3 ? "dotselected dot" : "dot"}
+                onClick={() => selectedDot(3)}
+              ></span>
+              <span
+                className={index === 4 ? "dotselected dot" : "dot"}
+                onClick={() => selectedDot(4)}
+              ></span>
+              <span
+                className={index === 5 ? "dotselected dot" : "dot"}
+                onClick={() => selectedDot(4)}
+              ></span>
+            </div>
+            <div>
+              <button
+                className="btn slide-arrow"
+                disabled={index === 5 && "false"}
+                onClick={() => nextSlide(+1)}
+              >
+                {">"}
+              </button>
             </div>
           </div>
         </div>
-      </div>
-      <div className="slide-controler">
-        <div>
-          <button className="btn slide-arrow">{"<"}</button>
-        </div>
-        <div>
-          <span className="dot"></span>
-          <span className="dot"></span>
-          <span className="dot"></span>
-          <span className="dot"></span>
-          <span className="dot"></span>
-        </div>
-        <div>
-          <button className="btn slide-arrow">{">"}</button>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
