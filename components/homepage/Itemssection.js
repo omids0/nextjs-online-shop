fde6b5cc1258130164c457c19fe9050fa196a74e
omids0 <soheilniao@gmail.com>
2022-03-30 20:06:01 +0430

    item section basics created

--- components/homepage/Itemssection.js
+++ components/homepage/Itemssection.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from "react";
+import Fade from "react-reveal/Fade";
+import Image from "next/image";
 
-export default function Itemssection({products}) {
+export default function Itemssection({ products }) {
   return (
-      <div className='homepage-items-section'>items</div>
-  )
+    <div className="homepage-items-section">
+      {products.map((product) => (
+        <div key={product._id} className="each-product-item">
+          <Fade bottom>
+            <div className="each-product-item-inner">
+              <div>
+                <Image
+                  src={product.selectedImage}
+                  alt={product.name}
+                  layout="responsive"
+                  width={500}
+                  height={500}
+                />
+              </div>
+              <div>
+                <p>{product.name}</p>
+              </div>
+            </div>
+          </Fade>
+        </div>
+      ))}
+    </div>
+  );
 }
