04ca10e4ce1b611e05276c53b34a8013e2722051
omids0 <soheilniao@gmail.com>
2022-04-02 01:34:06 +0430

    about component desined

--- components/homepage/Aboutsite.js
+++ components/homepage/Aboutsite.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export default function Aboutsite() {
   return (
-      <div className='homepage-about-site'>about site and insta</div>
-  )
+    <div className="homepage-about-site">
+      <div className='footer-item-centering'>
+        <i className="bi bi-clock-history"></i>
+        <p>پشتیبانی ۲۴ ساعته</p>
+      </div>
+      <div className='footer-item-centering'>
+        <i className="bi bi-bookmark-check"></i>
+        <p>امکان بازگشت کالا ۷ روز پس از تحویل</p>
+      </div>
+      <div className='footer-item-centering'>
+        <i className="bi bi-truck"></i>
+        <p>ارسال سریع بین ۳ الی ۷ روز کاری</p>
+      </div>
+    </div>
+  );
 }
