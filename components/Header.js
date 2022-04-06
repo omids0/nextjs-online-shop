f33414734b1a46474b7e18f9a9dc6247f75b0a9f
omids0 <soheilniao@gmail.com>
2022-04-06 15:38:40 +0430

    hamburg menu added

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserHistoryBasketAction } from "../redux/actions/basketActions";
 
@@ -8,6 +8,9 @@ export default function Header() {
   const basket = useSelector((state) => state.addToBasketReducer.basketItems);
 
   const dispatch = useDispatch();
+
+  const [hamburMenu, sethamburMenu] = useState(false);
+
   useEffect(() => {
     const basket = localStorage.getItem("omidshopbasket")
       ? JSON.parse(localStorage.getItem("omidshopbasket"))
@@ -15,26 +18,48 @@ export default function Header() {
     dispatch(getUserHistoryBasketAction(basket));
   }, []);
 
+  const showHamburgerMenu = () => {
+    sethamburMenu(!hamburMenu);
+  };
+
   return (
     <header>
       <div className="header-first-section">
-        <Link href="/admin" passHref>
-          <button className="btn hamburger-menu">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="currentColor"
-              className="bi bi-list"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fillRule="evenodd"
-                d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-              />
-            </svg>
-          </button>
-        </Link>
+        <button className="btn hamburger-menu" onClick={showHamburgerMenu}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="currentColor"
+            className="bi bi-list"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+            />
+          </svg>
+        </button>
+        {hamburMenu && (
+          <div className="hamburger-menu-up">
+            <div className="hamburger-menu-up1">
+              <button className="btn" onClick={() => sethamburMenu(false)}>
+                <i className="bi bi-x btn-x"></i>
+              </button>
+              <p>Omid Shop</p>
+            </div>
+            <div className="hamburger-menu-up2">
+              <Link href="/admin" passHref>
+                <p>دسترسی ادمین</p>
+              </Link>
+              <Link href="/login" passHref>
+                <p>ورود یا ثبت نام</p>
+              </Link>
+              <p>فرصت های شغلی</p>
+              <p>درباره سایت</p>
+            </div>
+          </div>
+        )}
         <Link href="/" passHref>
           <Image
             src="/images/—Pngtree—online shopping sale vector _3548364.png"
