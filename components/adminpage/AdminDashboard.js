4c3de3f23c5eecb0b977ce6efb2f7f00171e7f3b
omids0 <soheilniao@gmail.com>
2022-04-07 22:46:53 +0430

    admin dashboard compleated

--- components/adminpage/AdminDashboard.js
+++ components/adminpage/AdminDashboard.js
@@ -1,44 +1,92 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Loading from "../Loading";
 
 export default function AdminDashboard() {
+  const [users, setusers] = useState([]);
+  const [orders, setorders] = useState([]);
+  const [products, setproducts] = useState([]);
+
+  useEffect(() => {
+    fetchUsers();
+    fetchOrders();
+    fetchProducts();
+  }, []);
+
+  const fetchUsers = async () => {
+    await fetch("/api/users/")
+      .then((response) => response.json())
+      .then((data) => setusers(data));
+  };
+
+  const fetchOrders = async () => {
+    await fetch("/api/orders/")
+      .then((response) => response.json())
+      .then((data) => setorders(data));
+  };
+
+  const fetchProducts = async () => {
+    await fetch("/api/products/")
+      .then((response) => response.json())
+      .then((data) => setproducts(data));
+  };
+
   return (
-    <div className="admin-dashboard-container">
-      <div className="admin-dashboard-division admin-dashboard-users">
-        <h3>تعداد کل کاربران</h3>
-        <p>10 نفر</p>
-      </div>
-      <div className="admin-dashboard-division admin-dashboard-products">
-        <h3>کل محصولات</h3>
-        <p>16 عدد</p>
-      </div>
-      <div className="admin-dashboard-division admin-dashboard-products">
-        <h3>کل محصولات موجود</h3>
-        <p>14 عدد</p>
-      </div>
-      <div className="admin-dashboard-division admin-dashboard-orders">
-        <h3>کل سفارشات</h3>
-        <p>1124 عدد</p>
-      </div>
-      <div className="admin-dashboard-division admin-dashboard-orders">
-        <h3>سفارشات معلق</h3>
-        <p>12 عدد</p>
-      </div>
-      <div className="admin-dashboard-division admin-dashboard-orders">
-        <h3>سفارشات ارسال به پست</h3>
-        <p>101 عدد</p>
-      </div>
-      <div className="admin-dashboard-division admin-dashboard-orders">
-        <h3>سفارشات داخل پست</h3>
-        <p>90 عدد</p>
-      </div>
-      <div className="admin-dashboard-division admin-dashboard-orders">
-        <h3>سفارشات تحویل داده شده</h3>
-        <p>500 عدد</p>
-      </div>
-      <div className="admin-dashboard-division admin-dashboard-orders">
-        <h3>سفارشات مرجوعی</h3>
-        <p>2 عدد</p>
-      </div>
+    <div>
+      {users.length > 0 && orders.length > 0 && products.length > 0 ? (
+        <div className="admin-dashboard-container">
+          <div className="admin-dashboard-division admin-dashboard-users">
+            <h3>تعداد کل کاربران</h3>
+            <p>{users.length} نفر</p>
+          </div>
+          <div className="admin-dashboard-division admin-dashboard-products">
+            <h3>کل محصولات</h3>
+            <p>{products.length} عدد</p>
+          </div>
+          <div className="admin-dashboard-division admin-dashboard-products">
+            <h3>کل محصولات موجود</h3>
+            <p>{products.filter((product) => product.qty > 0).length} عدد</p>
+          </div>
+          <div className="admin-dashboard-division admin-dashboard-orders">
+            <h3>کل سفارشات</h3>
+            <p>{orders.length} عدد</p>
+          </div>
+          <div className="admin-dashboard-division admin-dashboard-orders">
+            <h3>سفارشات معلق</h3>
+            <p>
+              {orders.filter((product) => product.confirmed === false).length}{" "}
+              عدد
+            </p>
+          </div>
+          <div className="admin-dashboard-division admin-dashboard-orders">
+            <h3>سفارشات ارسال به پست</h3>
+            <p>
+              {orders.filter((product) => product.sendToPost === true).length}{" "}
+              عدد
+            </p>
+          </div>
+          <div className="admin-dashboard-division admin-dashboard-orders">
+            <h3>سفارشات داخل پست</h3>
+            <p>
+              {orders.filter((product) => product.inPost === true).length} عدد
+            </p>
+          </div>
+          <div className="admin-dashboard-division admin-dashboard-orders">
+            <h3>سفارشات تحویل داده شده</h3>
+            <p>
+              {orders.filter((product) => product.deliverd === true).length} عدد
+            </p>
+          </div>
+          <div className="admin-dashboard-division admin-dashboard-orders">
+            <h3>سفارشات مرجوعی</h3>
+            <p>
+              {orders.filter((product) => product.noteAccept === true).length}{" "}
+              عدد
+            </p>
+          </div>
+        </div>
+      ) : (
+        <Loading />
+      )}
     </div>
   );
 }
