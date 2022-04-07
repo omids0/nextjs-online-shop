4d499c4a8e1fa5f61be8062ec7788210faa68745
omids0 <soheilniao@gmail.com>
2022-04-07 22:15:22 +0430

    api directory cleand

--- pages/api/hello.js
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import db from "../../utils/db/mongoose";
-
-export default function handler(req, res) {
-  // db.connect();
-  // db.disconnect();
-  res.status(200).json({ name: "John Doe" });
-}
--- pages/api/test.js
+++
@@ -1,3 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({ name: "Mongo DB is connected!" });
-}
