0df4efafef570c9c782e481a212949d6107ca76b
omids0 <soheilniao@gmail.com>
2022-04-10 13:32:14 +0430

    admin user edited

--- components/adminpage/AdminUsers.js
+++ components/adminpage/AdminUsers.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function AdminUsers() {
   return (
-    <div>
-      users
+    <div className='mt2'>
+      <h4>به دلیل امینت و حفظ شماره تماس کاربران، این کامپوننت طراحی نگردید.</h4>
     </div>
   )
 }
